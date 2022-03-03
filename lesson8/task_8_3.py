ff7eacda8bbcbac96cd47cd0e3ac2392fdfe1713
Valerulia <tryk@rambler.ru>
2022-03-03 23:21:53 +0300

    Добавлена задача 3 ПЗ8

--- lesson8/task_8_3.py
+++ lesson8/task_8_3.py
@@ -2,8 +2,7 @@ __author__ = 'Рейтер Валерия Борисовна'
 
 # 3. Написать декоратор для логирования(вывод в консоль) типов позиционных аргументов функции:
 # Примеры/Тесты:
-#
-#
+
 # def type_logger...
 #     ...
 #
@@ -38,13 +37,34 @@ __author__ = 'Рейтер Валерия Борисовна'
 # решить задачу для именованных аргументов
 # вывести имя функции
 
-def type_logger...
-#     ...
-#
-# @type_logger
-# def render_input(*args, **kwargs):
-#    return 1
-#
-# @type_logger
-# def calc_cube(x):
-#    return x ** 3
+
+def type_logger(func):
+    def wrapper(*args, **kwargs):
+        print(f"Call for: {func.__name__}")
+        arg_itog = ''
+        kwarg_itog = ''
+        for arg in args:
+            arg_itog += f"{arg}: {type(arg)}, "
+        for key, value in kwargs.items():
+            kwarg_itog += f"{key} = {value}: {type(value)}, "
+        if len(arg_itog) > 0:
+            print(arg_itog.rstrip(', '))
+        if len(kwarg_itog) > 0:
+            print(kwarg_itog.rstrip(', '))
+        result = func(*args, **kwargs)
+        return f"Rezult: {result} {type(result)}\n"
+    return wrapper
+
+
+@type_logger
+def render_input(*args, **kwargs):
+    return 1
+
+
+@type_logger
+def calc_cube(x):
+    return x ** 3
+
+
+print(render_input(1, a=2, b=True, c="q"))
+print(calc_cube(5))
